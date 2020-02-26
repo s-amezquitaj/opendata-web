8b0f6c9a3063b0d00eb3afbc07b1ec7a6ea13381
Pablo <jamezqu1@stevens.edu>
2020-02-25 22:12:03 -0500

    H

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 // Here's an example of what a sample page looks like.
 // See more: https://nextjs.org/learn/basics/getting-started/first-page
 
-export default () => (
-  <div>
-    <p>Hello DSC!</p>
-  </div>
-)
+//export default () => (
+//  <div>
+//    <p>Hello DSC!</p>
+//  </div>
+//)
+
+export default function Index() {
+  return (
+    <div>
+      <p>Hello DSC!</p>
+    </div>
+  );
+}
